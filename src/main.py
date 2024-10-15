3e52ffb451a72a13672818860ab58f4a36a58a80
Aram SEMO <aram.semo@asemo.pro>
2024-10-15 13:52:34 +0200

    beta V4

--- src/main.py
+++ src/main.py
@@ -7,17 +7,24 @@ import random
 import requests
 import functools
 import validators
-
+import logging
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Dict, List, AnyStr, Type
 from threading import Thread
 
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('domain_manager')
+
+
+# Custom Exception Classes
 class InvalidDomainException(Exception):
     """Custom exception to indicate an invalid domain name."""
     pass
 
 
+# Utility Functions
 def validate_domain(name: str):
     if not validators.domain(name):
         raise InvalidDomainException(f"The domain name '{name}' is not valid.")
@@ -28,13 +35,10 @@ def extract_subdomain(full_domain: str, base_domain: str) -> Optional[str]:
     if not validators.hostname(full_domain) or not validators.hostname(base_domain):
         return None
 
-    # Split the domains into parts
-    full_domain_parts = [part for part in full_domain.split('.') if part != '']
-    base_domain_parts = [part for part in base_domain.split('.') if part != '']
+    full_domain_parts = full_domain.split('.')
+    base_domain_parts = base_domain.split('.')
 
-    # Check if the full domain ends with the base domain
     if full_domain_parts[-len(base_domain_parts):] == base_domain_parts:
-        # Extract the subdomain parts
         subdomain_parts = full_domain_parts[:-len(base_domain_parts)]
         return '.'.join(subdomain_parts) if subdomain_parts else None
 
@@ -44,49 +48,35 @@ def extract_subdomain(full_domain: str, base_domain: str) -> Optional[str]:
     return None
 
 
+# Decorators
 def handle_api_errors(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except (ovh.exceptions.APIError, requests.RequestException) as exception:
-            # Handle exceptions that have response attributes
-            status_code = {
-                400: "Bad Request:",
-                401: "Unauthorized:",
-                403: "Forbidden:",
-                404: "Not Found:",
-                500: "Internal Server Error:",
-            }
             response = getattr(exception, 'response', None)
-
             if hasattr(response, 'status_code') and hasattr(response, 'url'):
-                print(status_code.get(response.status_code, "Unexpected API Error:"), response.url)
+                logger.error(f"API Error {response.status_code}: {response.url}")
             else:
-                print("Unexpected Error: No response object available.")
-
-            # Print the exception details and the traceback
-            print(f"An exception of type '{type(exception).__name__}' occurred: {exception}")
-
+                logger.error(f"Unexpected Error: {exception}")
     return wrapper
 
 
+# Abstract Base Classes
 class SubdomainProvider(ABC):
-    _providers = {}
-
-    def __init_subclass__(cls, *args, **kwargs):
-        cls._providers[cls.__name__.replace('Provider', '').upper()] = cls
-
-    @classmethod
-    def get_provider(cls, name, *args, **kwargs) -> 'SubdomainProvider':
-        provider_cls = cls._providers.get(name)
-        if not provider_cls:
-            raise ValueError(f"Provider '{name}' is not registered.")
-        return provider_cls(*args, **kwargs)
+    name: str
+    keys: List[AnyStr]
+    details: List[AnyStr] = ['domain_name', 'target']
 
     def __init__(self, domain_name: str, target: str):
         self.domain_name = validate_domain(domain_name)
         self.target = validate_domain(target)
+
+    def __init_subclass__(cls, *args, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        cls.name = cls.__name__.replace('Provider', '')
+        ProviderFactory.register_provider(cls.name.upper(), cls)
 
     @abstractmethod
     def add_subdomain(self, subdomain: str):
@@ -96,10 +86,77 @@ class SubdomainProvider(ABC):
     def remove_subdomain(self, subdomain: str):
         pass
 
+    def _log_action(self, action: str, subdomain: str):
+        logger.info(f"{action} subdomain '{subdomain}' on '{self.name}' for domain '{self.domain_name}' with target '{self.target}'.")
 
+
+# Environment Handler
+class EnvironmentManager:
+    @staticmethod
+    def _get_variables(prefix: str, variables: list, message: str = "required environment variables") -> Dict[str, str]:
+        result = {}
+        for var in variables:
+            result[var] = os.getenv(key=f'{prefix}_{var}'.upper(), default=None)
+
+        missing = [key for key, value in result.items() if not value]
+        if missing:
+            formatted_missing = ', '.join(missing)
+            formatted_pattern = '\n'.join([f'{prefix}_{key}'.upper() for key in missing])
+            raise ValueError(
+                f"Missing {message}: {formatted_missing}.\n"
+                f"Please ensure you set the environment variables as shown below:\n{formatted_pattern}"
+            )
+        return result
+
+    @classmethod
+    def get_provider_keys(cls, provider: Type[SubdomainProvider]) -> Dict[str, str]:
+        return cls._get_variables(
+            prefix=f'DNM_{provider.__name__.replace("Provider", "").upper()}',
+            variables=provider.keys,
+            message=f"API keys for provider '{provider.name}'"
+        )
+
+    @classmethod
+    def get_provider_details(cls, provider: Type[SubdomainProvider]) -> Dict[str, str]:
+        return cls._get_variables(
+            prefix='DNM',
+            variables=provider.details,
+            message="domain details"
+        )
+
+    @classmethod
+    def get_docker_configuration(cls, variables: List[AnyStr]) -> Dict[str, str]:
+        return cls._get_variables(
+            prefix='DNM_DOCKER',
+            variables=variables,
+            message="Docker configuration"
+        )
+
+
+class ProviderFactory:
+    _providers: Dict[str, type[SubdomainProvider]] = {}
+
+    @classmethod
+    def register_provider(cls, name: str, provider_cls):
+        cls._providers[name.upper()] = provider_cls
+
+    @classmethod
+    def get_provider(cls, name) -> SubdomainProvider:
+        provider_cls = cls._providers.get(name.upper())
+        if not provider_cls:
+            raise ValueError(f"Provider '{name}' is not registered.")
+
+        keys = EnvironmentManager.get_provider_keys(provider_cls)
+        domain_details = EnvironmentManager.get_provider_details(provider_cls)
+        return provider_cls(**domain_details, **keys)
+
+
+# Concrete Providers
 class OVHProvider(SubdomainProvider):
+    keys = ['application_key', 'application_secret', 'consumer_key']
+
     def __init__(self, application_key, application_secret, consumer_key, domain_name, target):
-        super().__init__(domain_name=domain_name, target=target)
+        super().__init__(domain_name, target)
         self.client = ovh.Client(
             endpoint='ovh-eu',
             application_key=application_key,
@@ -114,7 +171,7 @@ class OVHProvider(SubdomainProvider):
                          subDomain=subdomain,
                          target=self.target,
                          ttl=3600)
-        print(f"Successfully added subdomain '{subdomain}' to OVH for domain '{self.domain_name}' with target '{self.target}'.")
+        self._log_action(action="Added", subdomain=subdomain)
 
     @handle_api_errors
     def remove_subdomain(self, subdomain: str):
@@ -122,20 +179,21 @@ class OVHProvider(SubdomainProvider):
                                   fieldType='CNAME',
                                   subDomain=subdomain)
         for record_id in records:
-            # Adding exponential backoff to avoid overwhelming the API
             delay = random.uniform(0.1, 0.5)
             self.client.delete(f'/domain/zone/{self.domain_name}/record/{record_id}')
             time.sleep(delay)
-        print(f"Successfully removed subdomain '{subdomain}' from OVH for domain '{self.domain_name}'.")
+        self._log_action(action="Removed", subdomain=subdomain)
 
 
 class CloudflareProvider(SubdomainProvider):
-    def __init__(self, api_key, domain_name, target):
-        super().__init__(domain_name=domain_name, target=target)
+    keys = ['api_token']
+
+    def __init__(self, api_token, domain_name, target):
+        super().__init__(domain_name, target)
         self.base_url = "https://api.cloudflare.com/client/v4"
         self._session = requests.Session()
         self._session.headers.update({
-            "Authorization": f"Bearer {api_key}",
+            "Authorization": f"Bearer {api_token}",
             "Content-Type": "application/json"
         })
 
@@ -151,10 +209,8 @@ class CloudflareProvider(SubdomainProvider):
             "proxied": False
         }
         response = self._session.post(url, json=data)
-        if response.status_code >= 400:
-            print(f"HTTP Error {response.status_code}: {response.text}")
         response.raise_for_status()
-        print(f"Successfully added subdomain '{subdomain}' to Cloudflare for domain '{self.domain_name}' with target '{self.target}'.")
+        self._log_action(action="Added", subdomain=subdomain)
 
     @handle_api_errors
     def remove_subdomain(self, subdomain: str):
@@ -164,9 +220,7 @@ class CloudflareProvider(SubdomainProvider):
             url = f"{self.base_url}/zones/{zone_id}/dns_records/{record_id}"
             response = self._session.delete(url)
             response.raise_for_status()
-            print(f"Successfully removed subdomain '{subdomain}' from Cloudflare for domain '{self.domain_name}'.")
-        else:
-            print(f"Subdomain '{subdomain}' not found for removal.")
+            self._log_action(action="Removed", subdomain=subdomain)
 
     def _get_zone_id(self):
         url = f"{self.base_url}/zones"
@@ -189,6 +243,7 @@ class CloudflareProvider(SubdomainProvider):
         return None
 
 
+# Subdomain Manager
 class SubdomainManager:
     def __init__(self, provider: SubdomainProvider):
         self.provider = provider
@@ -198,48 +253,91 @@ class SubdomainManager:
         if subdomain:
             self.provider.add_subdomain(subdomain)
         else:
-            print(f"Invalid subdomain '{full_domain}' for domain '{self.provider.domain_name}'.")
+            logger.error(f"Invalid subdomain '{full_domain}' for domain '{self.provider.domain_name}'.")
 
     def remove_subdomain(self, full_domain: str):
         subdomain = extract_subdomain(full_domain, self.provider.domain_name)
         if subdomain:
             self.provider.remove_subdomain(subdomain)
         else:
-            print(f"Invalid subdomain '{full_domain}' for domain '{self.provider.domain_name}'.")
+            logger.error(f"Invalid subdomain '{full_domain}' for domain '{self.provider.domain_name}'.")
 
 
-class DockerEventListener:
+# Event Listener Interface
+class EventListener(ABC):
+    @abstractmethod
+    def listen(self):
+        pass
+
+
+# Docker Event Listener
+class DockerEventListener(EventListener):
     def __init__(self, subdomain_manager: SubdomainManager, base_url: str = 'unix://var/run/docker.sock'):
         self.subdomain_manager = subdomain_manager
         self.docker_client = docker.DockerClient(base_url=base_url)
 
     def listen(self):
+        listening_thread = Thread(target=self._event_listener, daemon=True)
+        listening_thread.start()
+        return listening_thread
+
+    def _event_listener(self):
+        try:
+            for event in self.docker_client.events(decode=True, filters={'type': 'container'}):
+                self._handle_event(event)
+        except Exception as e:
+            logger.error(f"Error while listening to Docker events: {e}")
+
+    def _handle_event(self, event):
         action_mapping = {
             'create': self.subdomain_manager.add_subdomain,
             'destroy': self.subdomain_manager.remove_subdomain
         }
 
-        def event_listener():
-            try:
-                for event in self.docker_client.events(decode=True, filters={'type': 'container'}):
-                    action = event['Action']
-                    labels = event['Actor']['Attributes']
-                    rule_label = labels.get('traefik.http.routers.web.rule', None)
-                    if rule_label and action in action_mapping:
-                        match = re.search(r"Host\(`(.+?)`\)", rule_label)
-                        if match:
-                            action_mapping[action](match.group(1))
-            except Exception as e:
-                print(f"Error while listening to Docker events: {e}")
+        action = event['Action']
+        labels = event['Actor']['Attributes']
+        rule_label = labels.get('traefik.http.routers.web.rule', None)
 
-        listening_thread = Thread(target=event_listener, daemon=True)
-        listening_thread.start()
-        return listening_thread
+        if rule_label and action in action_mapping:
+            match = re.search(r"Host\(`(.+?)`\)", rule_label)
+            if match:
+                action_mapping[action](match.group(1))
+
+
+# Event Manager
+class EventManager:
+    def __init__(self, listeners: List[EventListener]):
+        self.listeners = listeners
+
+    def start_listening(self):
+        for listener in self.listeners:
+            listener.listen()
+
+
+# Main Functionality
+def main():
+    provider_name = os.getenv('DNM_PROVIDER', 'OVH')
+    docker_base_url = os.getenv('DNM_DOCKER_BASE_URL', 'unix://var/run/docker.sock')
+
+    provider = ProviderFactory.get_provider(provider_name)
+    manager = SubdomainManager(provider=provider)
+
+    # Using EventManager for managing all listeners
+    docker_listener = DockerEventListener(subdomain_manager=manager, base_url=docker_base_url)
+
+    event_manager = EventManager(listeners=[docker_listener])
+    event_manager.start_listening()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.info("Docker Event Listener stopped.")
 
 
 def main_test():
     provider = ['OVH', 'CLOUDFLARE'][0]
-    provider = SubdomainProvider.get_provider(provider, os.getenv('DNM_OVH_APPLICATION_KEY'), os.getenv('DNM_OVH_APPLICATION_SECRET'), os.getenv('DNM_OVH_CONSUMER_KEY'), os.getenv('DNM_DOMAIN_NAME'), os.getenv('DNM_TARGET'))
+    provider = ProviderFactory.get_provider(provider)
     manager = SubdomainManager(provider)
 
     subdomain = 'test'
@@ -247,40 +345,8 @@ def main_test():
     manager.remove_subdomain(subdomain)
 
 
-def main():
-    # Select provider based on environment variable
-    provider_name = os.getenv('DNM_PROVIDER', 'OVH').upper()
-    docker_base_url = os.getenv('DNM_DOCKER_BASE_URL', 'unix://var/run/docker.sock')
-
-    domain_name = os.getenv('DNM_DOMAIN_NAME')
-    target = os.getenv('DNM_TARGET', domain_name)
-
-    if provider_name == 'OVH':
-        provider = SubdomainProvider.get_provider(
-            provider_name, os.getenv('DNM_OVH_APPLICATION_KEY'), os.getenv('DNM_OVH_APPLICATION_SECRET'), os.getenv('DNM_OVH_CONSUMER_KEY'), domain_name, target
-        )
-    elif provider_name == 'CLOUDFLARE':
-        provider = SubdomainProvider.get_provider(
-            provider_name, os.getenv('DNM_CLOUDFLARE_API_TOKEN'), domain_name, target
-        )
-    else:
-        raise ValueError(f"Provider '{provider_name}' is not supported.")
-
-    manager = SubdomainManager(provider=provider)
-
-    # Start Docker Event Listener
-    listener = DockerEventListener(subdomain_manager=manager, base_url=docker_base_url)
-    listener.listen()
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Docker Event Listener stopped.")
-
-
 if __name__ == '__main__':
     import dotenv
     dotenv.load_dotenv()
-    # main()
-    main_test()
+    # main_test()
+    main()
